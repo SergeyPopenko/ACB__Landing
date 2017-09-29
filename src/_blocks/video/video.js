022cf566a46f3ca43833a928c1dd57658494c4ce
SergeyPopenko <skino4ek@ukr.net>
2017-09-29 14:38:47 +0300

    video first

---
+++ src/_blocks/video/video.js
@@ -0,0 +1,14 @@
+var iframe = document.getElementById('video');
+
+// $f == Froogaloop
+var player = $f(iframe);
+
+var playButton = document.getElementById("play-button");
+playButton.addEventListener("click", function() {
+  player.api("play");
+});
+
+var pauseButton = document.getElementById("pause-button");
+pauseButton.addEventListener("click", function() {
+  player.api("pause");
+});
