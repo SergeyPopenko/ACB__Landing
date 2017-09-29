0ebe6dd212bfcd7eff38f00ed500e321764f75c6
pots1988bear@gmail.com <pots1988bear@gmail.com>
2017-09-29 18:17:53 +0300

    Create animation

---
+++ src/_blocks/map/map.js
@@ -0,0 +1,42 @@
+// Map
+if ($("#map").length) {
+  var style = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"lightness":"-3"},{"saturation":"-47"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"saturation":"0"},{"lightness":"0"}]},{"featureType":"road","elementType":"labels.text","stylers":[{"lightness":"-29"},{"weight":"0.01"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#aaaaaa"},{"visibility":"on"}]}],
+      coordinates = {
+        lat: 55.783438,
+        lng: 37.574591
+      },
+      map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: coordinates,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        draggable: true,
+        scrollwheel: true,
+        styles: style
+      });
+
+  marker = new google.maps.Marker({
+    position: coordinates,
+    title: "УСК 'Крылья Советов'",
+    zIndex: 1000
+  });
+
+  map.addListener("center_changed", function(){
+    map.panTo(coordinates);
+  });
+
+  (function(){
+    var flag = 1;
+    $(window).on("scroll", function(){
+      if (flag) {
+        if ($(this).scrollTop() + window.innerHeight - $(".map").outerHeight()/2 > $(".map").offset().top) {
+          flag = 0;
+          marker.setAnimation(google.maps.Animation.DROP);
+          marker.setMap(map);
+        }
+      }
+    });
+  })();
+}
---
+++ src/_blocks/parallax/parallax.js
@@ -0,0 +1,23 @@
+if ($("[data-paroller-factor]").length) {
+  function loadScript(src, callback) {
+    var head = document.getElementsByTagName("head")[0],
+        script = document.createElement("script");
+    done = false;
+    script.setAttribute("src", src);
+    script.onload = script.onreadstatechange = function() {
+      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+        done = true;
+        script.onload = script.onreadystatechange = null;
+        if (callback) {
+          callback();
+        }
+      }
+    }
+    head.insertBefore(script, head.firstChild);
+  }
+  if (window.innerWidth > 767) {
+    loadScript("/js/paroller.min.js", function() {
+      $(window).paroller();
+    });
+  }
+}
