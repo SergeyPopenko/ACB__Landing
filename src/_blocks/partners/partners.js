8d27004187bbe4e538212bde0543b756c254b16c
SergeyPopenko <skino4ek@ukr.net>
2017-10-05 15:32:12 +0300

    partners

--- src/_blocks/partners/partners.js
+++ src/_blocks/partners/partners.js
@@ -1,10 +1,41 @@
-if (window.innerWidth < 768) {
+// if (window.innerWidth < 481) {
+//   $(".partners__slider").slick({
+//     infinite: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     dots: false,
+//     prevArrow: $(".partners__slider-wrap .slider__btn--prev"),
+//     nextArrow: $(".partners__slider-wrap .slider__btn--next"),
+//   });
+// }
+
+function partnersSliderInnit() {
   $(".partners__slider").slick({
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
     dots: false,
-    prevArrow: $(".partners__wrap .slider__btn--prev"),
-    nextArrow: $(".partners__wrap .slider__btn--next"),
+    prevArrow: $(".partners__slider-wrap .slider__btn--prev"),
+    nextArrow: $(".partners__slider-wrap .slider__btn--next"),
   });
 }
+
+function partnersSliderDestroy() {
+  $(".partners__slider").slick("unslick")
+}
+
+$(window).on("resize", function(){
+  if ($(".partners__slider.slick-initialized.slick-slider").length) {
+    if (window.innerWidth > 480) {
+      partnersSliderDestroy();
+    }
+  } else if(window.innerWidth < 481){
+    partnersSliderInnit();
+  }
+});
+
+$(window).ready(function(){
+  if (window.innerWidth < 481) {
+    partnersSliderInnit();
+  }
+});
