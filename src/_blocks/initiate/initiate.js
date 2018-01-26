48d62a40e67ec7998acfc85c0a7d55ced62f6f30
Maxim <pots1988bear@gmail.com>
2018-01-26 15:18:05 +0200

    Change some scripts

--- src/_blocks/initiate/initiate.js
+++ src/_blocks/initiate/initiate.js
@@ -1,11 +1,14 @@
-(function(){
-  var flag = 1;
-  $(window).on("scroll", function(){
-    if (flag) {
-      if ($(this).scrollTop() + window.innerHeight - $(".initiate").outerHeight()/2 > $(".initiate").offset().top) {
-        flag = 0;
-        $(".initiate").addClass("active");
+if ($(".initiate").length) {
+  (function(){
+    var flag = 1;
+    $(window).on("scroll", function(){
+      if (flag) {
+        if ($(this).scrollTop() + window.innerHeight - $(".initiate").outerHeight()/2 > $(".initiate").offset().top) {
+          flag = 0;
+          $(".initiate").addClass("active");
+        }
       }
-    }
-  });
-})();
+    });
+  })();
+}
+
